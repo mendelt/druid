136da805115ee2cf205bcc91ddb30a44085d789d
Mendelt Siebenga <msiebenga@gmail.com>
2019-12-28 15:45:47 +0100

    comment out button for now
    The compiler complains about the S type parameter of the Label in
    the Button widget. This wont be part of the implementation anyway.

--- druid/src/widget/button.rs
+++ druid/src/widget/button.rs
@@ -1,144 +1,144 @@
-// Copyright 2018 The xi-editor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// // Copyright 2018 The xi-editor Authors.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
 
-//! A button widget.
+// //! A button widget.
 
-use crate::kurbo::{Point, RoundedRect, Size};
-use crate::piet::{LinearGradient, RenderContext, UnitPoint};
-use crate::theme;
-use crate::widget::{Align, Label, LabelText, SizedBox};
-use crate::{BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx, Widget};
+// use crate::kurbo::{Point, RoundedRect, Size};
+// use crate::piet::{LinearGradient, RenderContext, UnitPoint};
+// use crate::theme;
+// use crate::widget::{Align, Label, LabelText, SizedBox};
+// use crate::{BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx, Widget};
 
-/// A button with a text label.
-pub struct Button<T> {
-    label: Label<T>,
-    /// A closure that will be invoked when the button is clicked.
-    action: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
-}
+// /// A button with a text label.
+// pub struct Button<T> {
+//     // label: Label<T>,
+//     /// A closure that will be invoked when the button is clicked.
+//     action: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
+// }
 
-impl<T: Data + 'static> Button<T> {
-    /// Create a new button. The closure provided will be called when the button
-    /// is clicked.
-    pub fn new<S>(
-        text: impl Into<LabelText<T>>,
-        action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
-    ) -> Button<T> {
-        Button {
-            label: Label::new(text).align(UnitPoint::CENTER),
-            action: Box::new(action),
-        }
-    }
+// impl<T: Data + 'static> Button<T> {
+//     /// Create a new button. The closure provided will be called when the button
+//     /// is clicked.
+//     pub fn new<S>(
+//         text: impl Into<LabelText<T>>,
+//         action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
+//     ) -> Button<T> {
+//         Button {
+//             // label: Label::new(text).align(UnitPoint::CENTER),
+//             action: Box::new(action),
+//         }
+//     }
 
-    /// Create a new button with a fixed size.
-    pub fn sized<S: 'static>(
-        text: impl Into<LabelText<T>>,
-        action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
-        width: f64,
-        height: f64,
-    ) -> impl Widget<T, S> {
-        Align::vertical(
-            UnitPoint::CENTER,
-            SizedBox::new(Button {
-                label: Label::new(text).align(UnitPoint::CENTER),
-                action: Box::new(action),
-            })
-            .width(width)
-            .height(height),
-        )
-    }
+//     /// Create a new button with a fixed size.
+//     pub fn sized<S: 'static>(
+//         text: impl Into<LabelText<T>>,
+//         action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
+//         width: f64,
+//         height: f64,
+//     ) -> impl Widget<T, S> {
+//         Align::vertical(
+//             UnitPoint::CENTER,
+//             SizedBox::new(Button {
+//                 // label: Label::new(text).align(UnitPoint::CENTER),
+//                 action: Box::new(action),
+//             })
+//             .width(width)
+//             .height(height),
+//         )
+//     }
 
-    /// A function that can be passed to `Button::new`, for buttons with no action.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use druid::widget::Button;
-    ///
-    /// let button = Button::<u32>::new("hello", Button::noop);
-    /// ```
-    pub fn noop(_: &mut EventCtx, _: &mut T, _: &Env) {}
-}
+//     /// A function that can be passed to `Button::new`, for buttons with no action.
+//     ///
+//     /// # Examples
+//     ///
+//     /// ```
+//     /// use druid::widget::Button;
+//     ///
+//     /// let button = Button::<u32>::new("hello", Button::noop);
+//     /// ```
+//     pub fn noop(_: &mut EventCtx, _: &mut T, _: &Env) {}
+// }
 
-impl<T: Data, S> Widget<T, S> for Button<T> {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, style_parent: &mut S, env: &Env) {
-        match event {
-            Event::MouseDown(_) => {
-                ctx.set_active(true);
-                ctx.invalidate();
-            }
-            Event::MouseUp(_) => {
-                if ctx.is_active() {
-                    ctx.set_active(false);
-                    ctx.invalidate();
-                    if ctx.is_hot() {
-                        (self.action)(ctx, data, env);
-                    }
-                }
-            }
-            Event::HotChanged(_) => {
-                ctx.invalidate();
-            }
-            _ => (),
-        }
-    }
+// impl<T: Data, S> Widget<T, S> for Button<T> {
+//     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, style_parent: &mut S, env: &Env) {
+//         match event {
+//             Event::MouseDown(_) => {
+//                 ctx.set_active(true);
+//                 ctx.invalidate();
+//             }
+//             Event::MouseUp(_) => {
+//                 if ctx.is_active() {
+//                     ctx.set_active(false);
+//                     ctx.invalidate();
+//                     if ctx.is_hot() {
+//                         (self.action)(ctx, data, env);
+//                     }
+//                 }
+//             }
+//             Event::HotChanged(_) => {
+//                 ctx.invalidate();
+//             }
+//             _ => (),
+//         }
+//     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, old_data: Option<&T>, data: &T, env: &Env) {
-        self.label.update(ctx, old_data, data, env)
-    }
+//     fn update(&mut self, ctx: &mut UpdateCtx, old_data: Option<&T>, data: &T, env: &Env) {
+//         // self.label.update(ctx, old_data, data, env)
+//     }
 
-    fn layout(
-        &mut self,
-        layout_ctx: &mut LayoutCtx,
-        bc: &BoxConstraints,
-        data: &T,
-        env: &Env,
-    ) -> Size {
-        bc.debug_check("Button");
+//     fn layout(
+//         &mut self,
+//         layout_ctx: &mut LayoutCtx,
+//         bc: &BoxConstraints,
+//         data: &T,
+//         env: &Env,
+//     ) -> Size {
+//         bc.debug_check("Button");
 
-        self.label.layout(layout_ctx, bc, data, env)
-    }
+//         // self.label.layout(layout_ctx, bc, data, env)
+//     }
 
-    fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &T, env: &Env) {
-        let is_active = paint_ctx.is_active();
-        let is_hot = paint_ctx.is_hot();
+//     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &T, env: &Env) {
+//         let is_active = paint_ctx.is_active();
+//         let is_hot = paint_ctx.is_hot();
 
-        let rounded_rect =
-            RoundedRect::from_origin_size(Point::ORIGIN, paint_ctx.size().to_vec2(), 4.);
-        let bg_gradient = if is_active {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_LIGHT), env.get(theme::BUTTON_DARK)),
-            )
-        } else {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_DARK), env.get(theme::BUTTON_LIGHT)),
-            )
-        };
+//         let rounded_rect =
+//             RoundedRect::from_origin_size(Point::ORIGIN, paint_ctx.size().to_vec2(), 4.);
+//         let bg_gradient = if is_active {
+//             LinearGradient::new(
+//                 UnitPoint::TOP,
+//                 UnitPoint::BOTTOM,
+//                 (env.get(theme::BUTTON_LIGHT), env.get(theme::BUTTON_DARK)),
+//             )
+//         } else {
+//             LinearGradient::new(
+//                 UnitPoint::TOP,
+//                 UnitPoint::BOTTOM,
+//                 (env.get(theme::BUTTON_DARK), env.get(theme::BUTTON_LIGHT)),
+//             )
+//         };
 
-        let border_color = if is_hot {
-            env.get(theme::BORDER_LIGHT)
-        } else {
-            env.get(theme::BORDER)
-        };
+//         let border_color = if is_hot {
+//             env.get(theme::BORDER_LIGHT)
+//         } else {
+//             env.get(theme::BORDER)
+//         };
 
-        paint_ctx.stroke(rounded_rect, &border_color, 2.0);
+//         paint_ctx.stroke(rounded_rect, &border_color, 2.0);
 
-        paint_ctx.fill(rounded_rect, &bg_gradient);
+//         paint_ctx.fill(rounded_rect, &bg_gradient);
 
-        self.label.paint(paint_ctx, data, env);
-    }
-}
+//         // self.label.paint(paint_ctx, data, env);
+//     }
+// }
