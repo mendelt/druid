5b87b65acbdcdcbe1fce2df02f17c667b5840e34
Mendelt Siebenga <msiebenga@gmail.com>
2020-01-06 22:33:55 +0100

    Templatable button in pseudocode
    The template T: Data is the ButtonState
      this way the button template event handlers can set the eventual button state
      this can be useful when only part of a button is clicable for example.
    
    The "template" is a function that returns a Widget<ButtonState> dynamically

--- druid/src/widget/button.rs
+++ druid/src/widget/button.rs
@@ -14,71 +14,57 @@
 
 //! A button widget.
 
-use crate::kurbo::{Point, RoundedRect, Size};
-use crate::theme;
-use crate::widget::{Label, LabelText};
+use crate::kurbo::Size;
+use crate::widget::{Label, LabelText, Container};
 use crate::{
-    BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, LinearGradient, PaintCtx, RenderContext,
-    UnitPoint, UpdateCtx, Widget,
+    BoxConstraints, BoxedWidget, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx, Widget,
 };
 
 /// A button with a text label.
 pub struct Button<T> {
-    label: Label<T>,
     /// A closure that will be invoked when the button is clicked.
-    action: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
+    on_clicked: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
+    template: Box<dyn Fn(&mut EventCtx, &mut T) -> Widget<ButtonState>>,
+    inner: BoxedWidget<ButtonState>,
+}
+
+#[derive(Clone, Data)]
+struct ButtonState {
+    clicked: bool,
 }
 
 impl<T: Data + 'static> Button<T> {
-    /// Create a new button. The closure provided will be called when the button
-    /// is clicked.
-    pub fn new(
-        text: impl Into<LabelText<T>>,
-        action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
-    ) -> Button<T> {
+    pub fn new(tempate: impl Fn(&ButtonState, &Env) ) -> Self {
+
+    }
+
+    /// Create a new textbutton
+    pub fn textButton(text: impl Into<LabelText>) -> Button<T> {
         Button {
-            label: Label::new(text).align(UnitPoint::CENTER),
-            action: Box::new(action),
+            template: |state, env| {
+                // TODO: Determine background brush based on state.clicked
+                Container::new(Label::new(text))
+                    .border(brush: impl Into<PaintBrush>, width: f64)
+                    .background(brush: impl Into<PaintBrush>)
+            },
+            action: None
         }
     }
 
-    /// A function that can be passed to `Button::new`, for buttons with no action.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use druid::widget::Button;
-    ///
-    /// let button = Button::<u32>::new("hello", Button::noop);
-    /// ```
-    pub fn noop(_: &mut EventCtx, _: &mut T, _: &Env) {}
+    pub fn on_clicked(self, handler: impl Fn(&mut EventCtx, &mut T, &Env) + 'static) -> Button<T> {
+        self.on_clicked = Some(handler);
+        self
+    }
 }
 
 impl<T: Data> Widget<T> for Button<T> {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
-        match event {
-            Event::MouseDown(_) => {
-                ctx.set_active(true);
-                ctx.invalidate();
-            }
-            Event::MouseUp(_) => {
-                if ctx.is_active() {
-                    ctx.set_active(false);
-                    ctx.invalidate();
-                    if ctx.is_hot() {
-                        (self.action)(ctx, data, env);
-                    }
-                }
-            }
-            Event::HotChanged(_) => {
-                ctx.invalidate();
-            }
-            _ => (),
-        }
+        self.inner.event(ctx, event, &mut self.state, env)
+        // TODO: determine if state has changed, act accordingly
     }
 
     fn update(&mut self, ctx: &mut UpdateCtx, old_data: Option<&T>, data: &T, env: &Env) {
-        self.label.update(ctx, old_data, data, env)
+        self.inner.update(ctx, None, &self.state, env)
     }
 
     fn layout(
@@ -90,39 +76,12 @@ impl<T: Data> Widget<T> for Button<T> {
     ) -> Size {
         bc.debug_check("Button");
 
-        self.label.layout(layout_ctx, bc, data, env)
+        self.inner.layout(layout_ctx, bc, &self.state, env)
     }
 
     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &T, env: &Env) {
-        let is_active = paint_ctx.is_active();
-        let is_hot = paint_ctx.is_hot();
-
-        let rounded_rect =
-            RoundedRect::from_origin_size(Point::ORIGIN, paint_ctx.size().to_vec2(), 4.);
-        let bg_gradient = if is_active {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_LIGHT), env.get(theme::BUTTON_DARK)),
-            )
-        } else {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_DARK), env.get(theme::BUTTON_LIGHT)),
-            )
-        };
-
-        let border_color = if is_hot {
-            env.get(theme::BORDER_LIGHT)
-        } else {
-            env.get(theme::BORDER)
-        };
-
-        paint_ctx.stroke(rounded_rect, &border_color, 2.0);
-
-        paint_ctx.fill(rounded_rect, &bg_gradient);
-
-        self.label.paint(paint_ctx, data, env);
+        // Dynamically recreate inner, then repaint it.
+        self.inner = WidgetPod::new(self.template(&self.state, env));
+        self.inner.paint(paint_ctx, &self.state, env)
     }
 }
